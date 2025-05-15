9121719a69b4aab8b3794cd916ef165f9452abfa
Lukasz <lukasz.mielewczyk1@gmail.com>
2025-05-15 15:29:43 +0200

    modify readme

--- startup1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y curl apt-transport-https
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
-# Start your application
-gunicorn --bind=0.0.0.0:8000 --timeout 600 app:app
